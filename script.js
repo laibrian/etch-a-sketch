d64fb722b7764742e0ebdd416d2bf55fc8a4c8bb
Brian Lai <laivbrian@gmail.com>
2022-07-11 14:47:17 -0500

    Add option to generate user entered grid size

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 const grid = document.querySelector('#grid');
+const gridSizeBtn = document.querySelector('#gridSizeBtn');
+//const status = document.querySelector('#status');
 
-let size = 16;
+gridSizeBtn.addEventListener('click', getGridSize);
+
+let size = 16;  // default grid height and width size
+
+function getGridSize() {
+    let text;
+    size = prompt("Enter the number of squares per side for the new grid (from 16 - 100):", "16");
+    if (size == null || size == "" || size < 16 || size > 100) {
+        size = 16;
+        text = "Using default number of squares: 16.";
+    } else {
+        text = "Creating new grid with " + size + " squares per side...";
+    }
+    document.querySelector('#status').textContent = text;
+    grid.innerHTML = '';
+    createGrid(size);
+}
 
 function createGrid(size) {
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
